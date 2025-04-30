94909e35bb3c8e5969ef9bf28197569c82be335d
Abhinav <abhinavchaudhary2014@gmail.com>
2025-05-01 01:30:57 +0530

    Test added for product creation

--- src/test/java/com/meAC/product/ProductServiceApplicationTests.java
+++ src/test/java/com/meAC/product/ProductServiceApplicationTests.java
@@ -1,13 +1,52 @@
 package com.meAC.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI="http://localhost";
+		RestAssured.port = port;
+	}
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+					"name":"iphone 15",
+					"description":"iphone15 is a smartphone from apple",
+					"price": 1000
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.post("/api/products")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name",Matchers.equalTo("iphone 15"))
+				.body("description",Matchers.equalTo("iphone15 is a smartphone from apple"))
+				.body("price",Matchers.equalTo(1000));
 	}
 
 }
